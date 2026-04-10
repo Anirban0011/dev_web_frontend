fa6fd44746333e5c747ee9c8b2c8b8b405fdd07a
Anirban0011 <bhattandbhatt.999@gmail.com>
2026-04-10 13:59:58 +0530

    upd login test workflow

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,18 +3,21 @@ import { test, expect } from '@playwright/test'
 
 test('user can login successfully', async ({ page, context }) => {
     await context.clearCookies()
+
+     let loginStatus = null
+    page.on('response', r => {
+        if (r.url().includes('login')) {
+            loginStatus = r.status()
+            console.log('LOGIN STATUS:', loginStatus)
+        }
+    })
+    
     await page.goto('http://localhost:5173')
 
     await page.getByTestId("ham-menu-btn").click()
     await page.getByTestId("login-btn").click()
 
     await expect(page).toHaveURL(/login/)
-
-    page.on('response', r => {
-  if (r.url().includes('login')) {
-    console.log('LOGIN STATUS:', r.status())
-    }
-    })
 
     await page.getByTestId("username-email-input").fill(process.env.TEST_USERNAME)
     await page.getByTestId("username-pass-input").fill(process.env.TEST_PASSWORD)
