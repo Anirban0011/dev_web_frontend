4fab9624ca953fd28fa0c08a65b828d8b6b8b222
Anirban <bhattandbhatt.999@gmail.com>
2026-04-11 13:40:59 +0530

    test : ci check 20 (#23)

--- scripts/gitcommit.ps1
+++ scripts/gitcommit.ps1
@@ -1,10 +1,3 @@
 function ship($branch, $message) {
-    git checkout -b $branch
-    git add -A
-    git commit -m $message
-    git push origin $branch
-    git checkout master
-    git branch -D $branch
-
     Write-Host " 🚀 Deployment initiated! " -ForegroundColor White -BackgroundColor Cyan
 }
